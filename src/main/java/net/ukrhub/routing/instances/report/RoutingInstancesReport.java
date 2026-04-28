4dba28d16a36eb101606111ffb7e2e0b45e2f656
Alexander Russkih <oldengremlin@gmail.com>
2026-04-28 08:20:26 +0300

    Initi empty code

---
+++ src/main/java/net/ukrhub/routing/instances/report/RoutingInstancesReport.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package net.ukrhub.routing.instances.report;
+
+/**
+ *
+ * @author olden
+ */
+public class RoutingInstancesReport {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
